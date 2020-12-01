5323b981570481a73d3bfe7dc1297b763d4109d0
Nathan Kane <kaneitalia.rana@gmail.com>
2020-12-01 13:54:54 -0800

    fixed a little typo

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,11 +1,13 @@
 class List < ApplicationRecord
   belongs_to :board
 
-  def self.single_list(board_id, list_id)
+  def self.single_list(user_id, list_id)
     List.find_by_sql(["
       SELECT * 
       FROM lists AS l
       WHERE l.id = ? AND l.board_id = ?
-    ", list_id, board_id]).first
+    ", list_id, user_id]).first
   end
 end
+# where is says WHERE l.id = ? AND l.board_id = ? is where you access the list through the board,
+    # and then the user_id param is where you access current user.
